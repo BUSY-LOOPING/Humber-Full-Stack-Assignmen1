a4931ab25f8931cef5d6cf477d23735b57777dbe
Dhruv <dhruvyadav2905@gmail.com>
2025-07-08 00:13:16 -0400

    api endpoints

--- modules/api/router.js
+++ modules/api/router.js
@@ -1,14 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import {Product} from '../../components/models/product.js';
-import {Category} from '../../components/models/category.js';
+import {getAllProducts, Product} from '../../components/models/product.js';
+import {Category, getAllCategories} from '../../components/models/category.js';
 
 
-//api endpoints
-// index.js (modified)
 router.get("/products", async (req, res) => {
     try {
-        const products = await Product.find().populate('category'); // Fetch and populate category data
+        const products = await getAllProducts(); 
         res.json(products);
     } catch (error) {
         console.error("Error fetching products:", error);
@@ -18,7 +16,7 @@ router.get("/products", async (req, res) => {
 
 router.get("/categories", async (req, res) => {
     try {
-        const categories = await Category.find();
+        const categories = await getAllCategories();
         res.json(categories);
     } catch (error) {
         console.error("Error fetching categories:", error);
